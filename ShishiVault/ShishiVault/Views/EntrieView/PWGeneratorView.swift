43c29caed02aa9de6dd0480d50b84bbec2669d73
Markus Wirtz <info@markuswirtz.net>
2024-11-21 23:10:39 +0100

    Anpassungen in der View PasswortGenerator

--- ShishiVault/ShishiVault/Views/EntrieView/PWGeneratorView.swift
+++ ShishiVault/ShishiVault/Views/EntrieView/PWGeneratorView.swift
@@ -8,16 +8,16 @@
 import SwiftUI
 
 struct PWGeneratorView: View {
-    @State private var lenght = 6.0
+    @State private var lenght = 8.0
     @State private var lowerCase: Bool = false
     @State private var upperCase: Bool = false
     @State private var numbers: Bool = false
     @State private var symbols: Bool = false
     
     private var sliderColor: Color {
-        if lenght < 6 {
+        if lenght <= 6 {
             return Color.ShishiColorRed_
-        } else if lenght >= 6 && lenght <= 10 {
+        } else if lenght > 6 && lenght <= 10 {
             return Color.orange
         } else if lenght > 10 {
             return Color.ShishiColorGreen
@@ -26,37 +26,51 @@ struct PWGeneratorView: View {
     }
     
     var body: some View {
-        VStack(alignment: .center) {
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 150, height: 4)
-                .foregroundStyle(Color.ShishiColorGray)
-                .padding(10)
-        }
+        RoundedRectangle(cornerRadius: 20)
+            .frame(width: 150, height: 4)
+            .foregroundStyle(Color.ShishiColorGray)
+            .padding(10)
         Spacer()
+        
         VStack {
-            Text("Passwort Generator")
+            Text("PASSWORT GENERATOR")
                 .ueberschriftenTextBold()
-                .padding()
+                .padding(.vertical, 20)
             
             HStack {
-                Text("Länge (\(lenght.formatted(.number)))")
+                Text("Länge (\(lenght.formatted(.number))) ")
                 Slider(value: $lenght, in: 1...32, step: 1.0) {
                     Text("Länge: \(lenght)")
                 }.tint(sliderColor)
             }
             
-            Toggle("Großbuchstaben", isOn: $upperCase)
-            Toggle("Kleinbuchstaben", isOn: $lowerCase)
-            Toggle("Zahlen", isOn: $numbers)
-            Toggle("Symbole", isOn: $symbols)
+            Divider()
             
+            Toggle("Großbuchstaben (A-Z)", isOn: $upperCase)
+                .padding(.vertical, 5)
+            Toggle("Kleinbuchstaben (a-z)", isOn: $lowerCase)
+                .padding(.vertical, 5)
+            Toggle("Zahlen (0-9)", isOn: $numbers)
+                .padding(.vertical, 5)
+            Toggle("Symbole (!@#$%^&*)", isOn: $symbols)
+                .padding(.vertical, 5)
             
+            Divider()
             
-            
-            
+            HStack {
+                Text("Security-Level")
+                    .padding(.vertical, 5)
+                
+                Spacer()
+                
+                Rectangle().frame(width: 20, height: 20)
+                Rectangle().frame(width: 20, height: 20)
+                Rectangle().frame(width: 20, height: 20)
+                Rectangle().frame(width: 20, height: 20)
+                Rectangle().frame(width: 20, height: 20)
+            }
             
             Button {
-                
                 
             } label: {
                 RoundedRectangle(cornerRadius: 25)
@@ -68,13 +82,25 @@ struct PWGeneratorView: View {
                         Text("Passwort erstellen")
                             .font(.title3).bold()
                             .foregroundColor(.white))
-            }
-            .padding(.horizontal).padding(.vertical, 5)
+            }.padding(.horizontal, 10)
+            
+            
+            HStack {
+                Image(systemName: "info.circle")
+                    .foregroundColor(Color.ShishiColorRed_)
+                    .padding(0)
+                Text("Hinweis: Zur Erstellung dieses Passwortes wird eine externe API angesteuert.")
+                    .customTextFieldText()
+            }.padding(5)
+            
             
         }.padding(.horizontal, 30)
         
         
-        .presentationDetents([.fraction(0.3)])
+        
+        
+        
+            .presentationDetents([.fraction(0.3)])
     }
 }
 
