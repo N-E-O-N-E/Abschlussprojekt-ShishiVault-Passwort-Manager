ce9bb0fc4ba04b22e190df403649b3a908616772
Markus Wirtz <info@markuswirtz.net>
2024-12-03 13:39:26 +0100

    Datensicherung

--- ShishiVault/ShishiVault/Views/HelpView/HelpView.swift
+++ ShishiVault/ShishiVault/Views/HelpView/HelpView.swift
@@ -10,45 +10,82 @@ import SwiftUI
 struct HelpView: View {
     @Environment(\.dismiss) private var dismiss
     
-    
     var body: some View {
-        HStack {
-            Text("OnBoarding")
-                .ueberschriftLargeBold()
-            Spacer()
-            Image("ShishiLogo_600")
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: 85)
-                .clipShape(.rect(cornerRadius: 15))
-                .padding(.horizontal, 25)
-        }.padding(.vertical, 15)
-        
-        
-        List(content: {
-            NavigationLink("Hilfe zu Punkt 1") {
-                
-            }
-        })
-        
-        
+        VStack {
+            HStack {
+                Text("Hilfe")
+                    .ueberschriftLargeBold()
+                Spacer()
+                Image("ShishiLogo_600")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 85)
+                    .clipShape(.rect(cornerRadius: 15))
+                    .padding(.horizontal, 25)
+            }.padding(.vertical, 15)
             
-            
-        
-        .navigationBarBackButtonHidden(true)
-        
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "chevron.backward")
-                        .foregroundColor(Color.ShishiColorBlue)
-                    Text("Zurück")
-                        .foregroundColor(Color.ShishiColorBlue)
+            ScrollView {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Divider()
+                        
+                        Text("\nÜber Shishi Vault")
+                            .ueberschriftenTextBold()
+                        Text("Beschreibungstextdsadhjksdhjsakhdsjadhsajdhsakjdhsakjdhsajkdhasjkdhk")
+                            .normalerText()
+                        Divider()
+                        
+                        Text("\nSicherheit")
+                            .ueberschriftenTextBold()
+                        Text("Beschreibungstextdsadhjksdhjsakhdsjadhsajdhsakjdhsakjdhsajkdhasjkdhk")
+                            .normalerText()
+                        Divider()
+                        
+                        Text("\nDatenexport")
+                            .ueberschriftenTextBold()
+                        Text("Beschreibungstextdsadhjksdhjsakhdsjadhsajdhsakjdhsakjdhsajkdhasjkdhk")
+                            .normalerText()
+                        Divider()
+                        
+                        Text("\nPasswort-Generator")
+                            .ueberschriftenTextBold()
+                        Text("Beschreibungstextdsadhjksdhjsakhdsjadhsajdhsakjdhsakjdhsajkdhasjkdhk")
+                            .normalerText()
+                        Divider()
+                        
+                        Text("\nPasswort-Verifikation")
+                            .ueberschriftenTextBold()
+                        Text("Beschreibungstextdsadhjksdhjsakhdsjadhsajdhsakjdhsakjdhsajkdhasjkdhk")
+                            .normalerText()
+                        Divider()
+                        
+                        
+                        
+                        
+                        
+                        
+                    }.padding(.horizontal, 20)
+                    Spacer()
                 }
             }
         }
+        
+        Spacer()
+        
+            .navigationBarBackButtonHidden(true)
+        
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                            .foregroundColor(Color.ShishiColorBlue)
+                        Text("Zurück")
+                            .foregroundColor(Color.ShishiColorBlue)
+                    }
+                }
+            }
     }
 }
 
