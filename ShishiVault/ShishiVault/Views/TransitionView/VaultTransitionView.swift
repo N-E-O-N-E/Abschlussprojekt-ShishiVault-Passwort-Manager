9436e9191a58fd7f0abe9d9953e19448eeef525d
Markus Wirtz <wirtz-markus@outlook.de>
2026-03-05 00:43:27 +0100

    logo and color added

--- ShishiVault/ShishiVault/Views/TransitionView/VaultTransitionView.swift
+++ ShishiVault/ShishiVault/Views/TransitionView/VaultTransitionView.swift
@@ -1,24 +1,30 @@
 import SwiftUI
 
+
 struct VaultTransitionView: View {
     @EnvironmentObject var shishiViewModel: ShishiViewModel
     
     var body: some View {
         ZStack {
-            Color.ShishiColorRed_.ignoresSafeArea() // Hintergrundfarbe erzwingen
+            Color.black.ignoresSafeArea().opacity(0.9)
             
             VStack(spacing: 20) {
+                Image("ShishiLogo_600")
+                    .resizable().scaledToFit().frame(width: 250).clipShape(RoundedRectangle(cornerRadius: 20))
+                    .padding()
+                                
                 ProgressView()
                     .progressViewStyle(CircularProgressViewStyle(tint: .white))
                     .scaleEffect(1.5)
+                    .padding()
                 
-                Text("Sicherheits-Check...")
+                Text("Sicherheits-Check läuft...")
                     .foregroundColor(.white)
                     .font(.headline)
+                    .padding()
             }
         }
         .onAppear {
-            // Erhöhe die Zeit auf 1.5 Sekunden zum Testen
             DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
                 withAnimation(.easeInOut) {
                     shishiViewModel.appState = .login
@@ -26,4 +32,11 @@ struct VaultTransitionView: View {
             }
         }
     }
+}
+
+#Preview {
+    let mockViewModel = ShishiViewModel()
+    
+    return VaultTransitionView()
+        .environmentObject(mockViewModel)
 }
