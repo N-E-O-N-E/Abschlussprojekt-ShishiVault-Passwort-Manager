b6149987c31b9226628c450cfa396a541eb5a292
Markus Wirtz <wirtz-markus@outlook.de>
2026-04-18 01:28:47 +0200

    refactor: remove unused RapidAPI key configuration from Info.plist and ConfigManager

--- ShishiVault/ShishiVault/Helper/ConfigManager.swift
+++ ShishiVault/ShishiVault/Helper/ConfigManager.swift
@@ -1,8 +1,7 @@
 import Foundation
-
 enum ConfigManager {
     /// Holt den Rapid-API Key aus der (lokalen/ignorierten) Secrets.swift Datei
     static var rapidAPIKey: String {
-        return Secrets.rapidAPIKey
+        Secrets.rapidAPIKey
     }
 }
